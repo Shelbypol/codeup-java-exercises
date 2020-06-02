1a2f266905367d9654ea53dd52b2ea59ac87b1be
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-02 18:57:07 -0500

    lecture notes

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -7,6 +7,8 @@ public class ConsoleExercises {
 //        System.out.printf("The value of pi is approximately %.2f", pi);
 
         Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner("\n");
+
 
 //        int num = sc.nextInt();
 //        System.out.printf("Your number is %d", num);
@@ -19,25 +21,29 @@ public class ConsoleExercises {
 //        System.out.println("Enter a sentence");
 //        String sentence1 = sc.nextLine();
 //        System.out.printf("Your sentence is: %s", sentence1);
-
-        System.out.println("In words enter the length and width of your classroom");
-        String stringLength = sc.nextLine();
-        String stringWidth = sc.nextLine();
-
-        int length = Integer.parseInt(stringLength);
-        int width = Integer.parseInt(stringWidth);
-
-        System.out.printf("The length of the classroom is: %s%nThe width of the classroom is: %s%nThe area of the classroom is: %d%nThe perimeter of the classroom is: %d", stringLength, stringWidth, length*width, ((length*2)+(width*2)));
-
-
-
-
-
-
-//        int length = sc.nextInt();
-//        int width = sc.nextInt();
 //
-//        System.out.printf("The length of the classroom is: %s%nThe width of the classroom is %s%nThe area of the classroom is %d%nThe perimeter of the classroom is %d", length, width, length*width, ((length*2)+(width*2)));
+//        System.out.println("Enter the length and width of your classroom");
+//        Integer length = Integer.parseInt(sc.nextLine());
+//        Integer width = Integer.parseInt(sc.nextLine());
+//
+//        int areaCalc = length * width;
+//        int widthCalc = (areaCalc)*2;
+//
+//        System.out.printf("The length of the classroom is: %d%nThe width of the classroom is: %d%nThe area of the classroom is: %d%nThe perimeter of the classroom is: %d", length, width, areaCalc, widthCalc);
+
+
+
+//        System.out.println("In words enter the length and width of your classroom");
+//        String stringLength = sc.nextLine();
+//        String stringWidth = sc.nextLine();
+//        String stringHeight = sc.nextLine();
+//
+//        double length = Double.parseDouble(stringLength);
+//        double width = Double.parseDouble(stringWidth);
+//        double height = Double.parseDouble(stringHeight);
+//
+//        System.out.printf("The length of the classroom is: %s%nThe width of the classroom is: %s%nThe height of the classroom is: %s%nThe area of the classroom is: %.2f%nThe perimeter of the classroom is: %.2f%nThe volume of the of the classroom is: %.2f", stringLength, stringWidth, stringHeight, length*width, ((length*2)+(width*2)), (length*width)*height);
+
 
 
     }
