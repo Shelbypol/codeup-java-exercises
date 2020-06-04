332f471ddb249f656adea2c53a00d5d49d28f7d4
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-04 14:18:13 -0500

    lecture

--- src/Bob.java
+++ src/Bob.java
@@ -21,14 +21,29 @@ public class Bob {
             userInput = sc.nextLine();
             if (userInput.endsWith("?")) {
                 System.out.println(sure);
+                System.out.println("^_^\n" +
+                                   "-|-\n" +
+                                   "/ \\\n");
             } else if (userInput.endsWith("!")) {
                 System.out.println(chillOut);
+                System.out.println("0_0\n" +
+                                   "~|~\n" +
+                                   "/ \\\n");
             } else if (userInput.equals("")) {
                 System.out.println(fine);
+                System.out.println("0_o\n" +
+                                   "<|>\n" +
+                                   "/ \\\n");
             }else if(userInput.equals("bye")){
-                System.out.print(bye);
+                System.out.println(bye);
+                System.out.println("0_0\n" +
+                                   "<|-'\n" +
+                                   "/ \\\n");
             } else {
                 System.out.println(whatever);
+                System.out.println("-_-\n" +
+                                   "<|>\n" +
+                                   "/ \\\n");
             }
         } while(!userInput.equals("bye"));
 
