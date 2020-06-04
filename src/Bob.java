855e6973284b99b2cd29048be651cee74542c82b
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-04 10:52:28 -0500

    finished bob talk

--- src/Bob.java
+++ src/Bob.java
@@ -6,29 +6,31 @@ public class Bob {
         String sure = "Sure";
         String chillOut = "Whoa, chill out!";
         String fine = "Fine. Be that way!";
+        String whatever = "Whatever...";
+        String bye = "Bye!";
 
         Scanner sc = new Scanner(System.in);
         System.out.println("This is Bob:  0_0\n" +
-                           "              -|-\n" +
-                           "              / \\\n" +
-                           "Talk to Bob:");
+                "              -|-\n" +
+                "              / \\\n" +
+                "Talk to Bob:");
         System.out.println("(...If you'd ever like to end your talk with Bob type: 'bye')");
-        String userInput = sc.next();
+        String userInput;
 
-        if (userInput.endsWith("?")) {
-            System.out.print(sure);
-        } else if (userInput.endsWith("!")) {
-            System.out.print(chillOut);
-        } else if (userInput.equalsIgnoreCase("")) {
-            System.out.print(fine);
-        }else if(userInput.equalsIgnoreCase("bye")){
-            System.out.print("Bye!");
-            System.exit(1);
-        }else{
-            System.out.print("Whatever...");
-        }
-
-
+        do {
+            userInput = sc.nextLine();
+            if (userInput.endsWith("?")) {
+                System.out.println(sure);
+            } else if (userInput.endsWith("!")) {
+                System.out.println(chillOut);
+            } else if (userInput.equals("")) {
+                System.out.println(fine);
+            }else if(userInput.equals("bye")){
+                System.out.print(bye);
+            } else {
+                System.out.println(whatever);
+            }
+        } while(!userInput.equals("bye"));
 
     }
 }
