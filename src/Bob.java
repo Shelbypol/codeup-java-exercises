4fb4338d519fb0349b8046a310a97d6d87a29d9d
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-04 14:23:12 -0500

    finished strings exercise

--- src/Bob.java
+++ src/Bob.java
@@ -21,27 +21,27 @@ public class Bob {
             userInput = sc.nextLine();
             if (userInput.endsWith("?")) {
                 System.out.println(sure);
-                System.out.println("^_^\n" +
+                System.out.print("^_^\n" +
                                    "-|-\n" +
                                    "/ \\\n");
             } else if (userInput.endsWith("!")) {
                 System.out.println(chillOut);
-                System.out.println("0_0\n" +
+                System.out.print("0_0\n" +
                                    "~|~\n" +
                                    "/ \\\n");
             } else if (userInput.equals("")) {
                 System.out.println(fine);
-                System.out.println("0_o\n" +
+                System.out.print("0_o\n" +
                                    "<|>\n" +
                                    "/ \\\n");
             }else if(userInput.equals("bye")){
                 System.out.println(bye);
-                System.out.println("0_0\n" +
+                System.out.print("0_0\n" +
                                    "<|-'\n" +
                                    "/ \\\n");
             } else {
                 System.out.println(whatever);
-                System.out.println("-_-\n" +
+                System.out.print("-_-\n" +
                                    "<|>\n" +
                                    "/ \\\n");
             }
