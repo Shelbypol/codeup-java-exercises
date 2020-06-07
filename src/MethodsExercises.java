af54c73b0e6e4c926f5ee40218a5bee699fd556e
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-07 10:34:24 -0500

    recursion mulitplication

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -25,19 +25,31 @@ public class MethodsExercises {
         return (a % b);
     }
 
-    public static int times(int a, int b){
-        int result = 1;
-        for(int i = 0; i <= a ; i++){
-            result = a * b
+    //bonus
+    private static int multiply(int x, int y) {
+        if (x == 0 || y == 0) {
+            return 0;
+        } else if (y > 0) {
+            System.out.println("x " + x);
+            System.out.println("y " + y);
+
+            int count = x + multiply(x, y - 1);
+
+            System.out.println("count " + count);
+
+            return count;
         }
+        return multiply(x, y);
     }
 
 
     public static void main(String[] args) {
-        System.out.println(addition(1, 2));
-        System.out.println(subtraction(3, 2));
-        System.out.println(multiplication(3, 2));
-        System.out.println(division(6, 0));
-        System.out.println(modulus(20, 2));
+//        System.out.println(addition(1, 2));
+//        System.out.println(subtraction(3, 2));
+//        System.out.println(multiplication(3, 2));
+//        System.out.println(division(6, 2));
+//        System.out.println(modulus(20, 2));
+//        System.out.println("times for loop " + times(2, 3));
+        System.out.println("answer "+ multiply(4,10));
     }
 }
