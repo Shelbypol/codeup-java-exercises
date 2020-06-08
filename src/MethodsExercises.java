4d9951e05bda02d39320a28fa4d0c3ed7b155830
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-08 14:21:12 -0500

    finished number 2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,30 +2,30 @@ import java.util.Scanner;
 
 public class MethodsExercises {
 
-    //addition
-    public static int addition(int a, int b) {
-        return (a + b);
-    }
-
-    //subtraction
-    public static int subtraction(int a, int b) {
-        return (a - b);
-    }
-
-    //multiplication
-    public static int multiplication(int a, int b) {
-        return (a * b);
-    }
-
-    //division
-    public static int division(int a, int b) {
-        return (a / b);
-    }
-
-    //modulus
-    public static int modulus(int a, int b) {
-        return (a % b);
-    }
+//    //addition
+//    public static int addition(int a, int b) {
+//        return (a + b);
+//    }
+//
+//    //subtraction
+//    public static int subtraction(int a, int b) {
+//        return (a - b);
+//    }
+//
+//    //multiplication
+//    public static int multiplication(int a, int b) {
+//        return (a * b);
+//    }
+//
+//    //division
+//    public static int division(int a, int b) {
+//        return (a / b);
+//    }
+//
+//    //modulus
+//    public static int modulus(int a, int b) {
+//        return (a % b);
+//    }
 
     //bonus
 //    public static int multiply(int x, int y) {
@@ -45,21 +45,43 @@ public class MethodsExercises {
 //    }
 
     //(2) Validate user input;
-    public static void getInteger() {
+    public static int getInteger(int min, int max) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number between 1 and 10: ");
-        int min =  Integer.parseInt(sc.nextLine());
-        int max =  Integer.parseInt(sc.nextLine());
+        System.out.println("Enter a number between 1 and 50: ");
+        int userInput = sc.nextInt();
 
-        if (( ((min > 0) && (min < 10)) && ((max > 0) && (max <= 10))  )){
-            System.out.println("Congrats, your input is within range!");
-
-        }else {
+        if (userInput > max || userInput < min) {
             System.out.println("Try Again:");
-            getInteger();
-
+            return getInteger(min, max);
         }
+        System.out.println("Congrats, your input is within range!");
+        return userInput;
+
     }
+
+    // (3) Factorial
+
+//    public static void factorial() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number and we will calculate the factorial of it!");
+//        String userAnswer;
+//
+//        do {
+//            int userInput = Integer.parseInt(sc.next());
+//            if ((userInput > 0) && (userInput <= 10)) {
+//                if(userInput > 1) {
+//                    long count = userInput * (userInput - 1);
+//                }
+//
+//            } else {
+//                factorial();
+//
+//            }
+//
+//            System.out.print("Would you like to continue? [y/N] ");
+//            userAnswer = sc.next();
+//        } while (userAnswer.equalsIgnoreCase("y"));
+//    }
 
 
     public static void main(String[] args) {
@@ -70,8 +92,8 @@ public class MethodsExercises {
 //        System.out.println(modulus(20, 2));
 //        System.out.println("times for loop " + times(2, 3));
 //        System.out.println("answer "+ multiply(4,10));
-        getInteger();
-
+        getInteger(1, 50);
+//        factorial();
 
 
     }
