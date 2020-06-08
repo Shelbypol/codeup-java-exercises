51c0e70021343fdcf24681483ea6f6e490b998af
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-08 09:12:22 -0500

    morning warmups

---
+++ src/MorningWarmups.java
@@ -0,0 +1,12 @@
+public class MorningWarmups {
+    public static void main(String[] args) {
+
+       // Create a loop that prints out the numbers one through fifty
+        for(int i = 0; i <=50; i+=5){
+            System.out.println(i);
+        }
+
+
+
+    }
+}
