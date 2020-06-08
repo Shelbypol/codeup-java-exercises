50b646abdd7d8c33ced73c88e1b0d8c226d536f2
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-08 16:39:14 -0500

    updated factorial

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,37 +1,37 @@
-import jdk.swing.interop.SwingInterOpUtils;
+//import jdk.swing.interop.SwingInterOpUtils;
+//
+//import java.util.Scanner;
 
-import java.util.Scanner;
+//public class HighLow {
 
-public class HighLow {
-
-    public static int random() {
-        return (int) (Math.random() * 100);
-    }
-
-    public static void highLow() {
-        int random = random();
-        System.out.println("random: " + random);
-        System.out.println("Guess a number 1 & 100!");
-    }
-        public static void game(){
-        Scanner sc = new Scanner(System.in);
-        int userInput = sc.nextInt();
-            if (userInput > random) {
-                System.out.println("LOWER");
-                game();
-            } else if (userInput < random) {
-                System.out.println("HIGHER");
-                game();
-            } else {
-                System.out.println("GOOD GUESS!");
-                highLow();
-            }
-        }
+//    public static int random() {
+//        return (int) (Math.random() * 100);
+//    }
+//
+//    public static void highLow() {
+//        int random = random();
+//        System.out.println("random: " + random);
+//        System.out.println("Guess a number 1 & 100!");
+//    }
+//        public static void game(){
+//        Scanner sc = new Scanner(System.in);
+//        int userInput = sc.nextInt();
+//            if (userInput > random) {
+//                System.out.println("LOWER");
+//                game();
+//            } else if (userInput < random) {
+//                System.out.println("HIGHER");
+//                game();
+//            } else {
+//                System.out.println("GOOD GUESS!");
+//                highLow();
+//            }
+//        }
 
 
 
-    public static void main(String[] args) {
-        highLow();
-        game();
-    }
-}
+//    public static void main(String[] args) {
+//        highLow();
+//        game();
+//    }
+//}
