e0ab281ff4c02d295359a47825c9cf755e27e833
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-10 11:49:23 -0500

    working

--- src/Person.java
+++ src/Person.java
@@ -1,25 +1,37 @@
 public class Person {
     private String name;
 
-    public String getName(){
-        return name;
-    }
-
+    //getter
     public String getName() {
 //TODO: return the person's name
         return this.name;
     }
 
+    //setter
     public void setName(String name) {
 //TODO: change the name property to the passed value
         this.name = name;
     }
 
+    //method
     public void sayHello() {
 //TODO: print a message to the console using the person's name
-        return
+        System.out.printf("Hello, \n%s!", getName());
     }
 
+    //constructor
+    public Person() {
+        Person.name = this.name;
+    }
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person();
+        p1.name = "shelby";
+        System.out.println(setName("shelby"));
+
+
+    }
 
 
 }
