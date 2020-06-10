709c1e7aaf9ab308d6a119610358cc0f770ed84d
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-10 11:02:01 -0500

    started oop exercise

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    private String name;
+
+    public String getName(Person n){
+//TODO: return the person's name
+        return Per
+    }
+
+    public void setName(String name){
+//TODO: change the name property to the passed value
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+    }
+
+}
