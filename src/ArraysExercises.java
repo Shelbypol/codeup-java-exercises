0fce3b4704bd8a8f24be53ccfa3f92e803e30793
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-14 14:06:52 -0500

    working through arrays

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,12 +2,29 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
+    //===1
+    public static String[] addPerson(String[] arrays, String p) {
+        String[] addPersonCopy = Arrays.copyOf(arrays, arrays.length + 1);
+        addPersonCopy[addPersonCopy.length - 1] = p;
+        return addPersonCopy;
+    }
+
+    //====== PSVM =======
     public static void main(String[] args) {
 
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(numbers));
+        //===1
+        String[] persons = {"Deena", "Shelby", "Brandi"};
 
-
-
+        String[] newPerson = addPerson(persons, "Leelah");
+        for (String person : newPerson) {
+            System.out.println(person);
+        }
+//        String[] newPerson = addPerson(persons, "Leelah");
     }
+
+
+
+
+
 }
+
---
+++ src/movies/movie.java
@@ -0,0 +1,16 @@
+package movies;
+
+public class movie {
+
+    private String name;
+    private String category;
+
+    public double getName() {
+        return this.radius = sc.nextDouble();
+    }
+
+    public void setName(double r) {
+        this.radius = r;
+    }
+
+}
