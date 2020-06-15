14edf8669eb1a48d7ecfafefd3c736ca4a3b5d3f
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-15 13:30:18 -0500

    working

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,10 +4,15 @@ public class ShapesTest {
 
     public static void main(String[] args) {
         Rectangle box1 = new Rectangle(5, 4);
+
+        System.out.println("=========");
         System.out.printf("This shape has a length of: %d and a width of %d.\n", box1.getLength(), box1.getWidth());
         System.out.println("Area: " + box1.getArea() + "\n" + "Perimeter: " + box1.getPerimeter());
 
+        System.out.println("=========");
+
         Rectangle box2 = new Square(5);
+
         System.out.printf("This shape has a length of: %d and a width of %d.\n", box2.getLength(), box2.getWidth());
         System.out.println("Area: " + box2.getArea() + "\n" + "Perimeter: " + box2.getPerimeter());
 
