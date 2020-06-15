7ebab3a03cf599e75e59f8092723631ad0b77dcd
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-15 13:25:57 -0500

    working through inheritance exercise

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,40 @@
+package shapes;
+
+public class Rectangle {
+
+    protected int length;
+    protected int width;
+
+    public Rectangle(){
+
+    }
+
+    public Rectangle(int l,int w){
+        this.length = l;
+        this.width = w;
+    }
+
+    //Methods
+    public int getArea(){
+        return (2 * this.length) + (2 * this.width);
+    }
+
+    public int getPerimeter(){
+        return this.length * this.width;
+    }
+
+
+    //GETTERS & SETTERS
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,15 @@
+package shapes;
+
+public class ShapesTest {
+
+    public static void main(String[] args) {
+        Rectangle box1 = new Rectangle(5, 4);
+        System.out.printf("This shape has a length of: %d and a width of %d.\n", box1.getLength(), box1.getWidth());
+        System.out.println("Area: " + box1.getArea() + "\n" + "Perimeter: " + box1.getPerimeter());
+
+        Rectangle box2 = new Square(5);
+        System.out.printf("This shape has a length of: %d and a width of %d.\n", box2.getLength(), box2.getWidth());
+        System.out.println("Area: " + box2.getArea() + "\n" + "Perimeter: " + box2.getPerimeter());
+
+    }
+}
