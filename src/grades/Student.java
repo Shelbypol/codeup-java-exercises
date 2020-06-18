213dada29b978c415225d24d7d59517043c9ca6d
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-18 13:18:21 -0500

    working exercise collections

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -3,39 +3,53 @@ import java.util.ArrayList;
 
 public class Student {
 
+    //PRIVATE PROPERTIES
     private String name;
-    private ArrayList<Double> grades = new ArrayList<>();
-    private Double gradeAverage;
+    private ArrayList<Integer> grades;
 
     //CONSTRUCTOR
-    public Student(String name, ArrayList<Double> grades){
+    public Student(String name, ArrayList<Integer> grades){
         this.name = name;
         this.grades = grades;
     }
-    //
-//    // returns the student's name
-//    public String getName();
-//    // adds the given grade to the grades property
-//    public void addGrade(int grade);
-    // returns the average of the students grades
+
+    //METHODS
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public Integer getGradeAverage() {
+        int sum = 0;
+        for(int grade : grades){
+            sum += grade;
+        }
+        return sum/grades.size();
+    }
+
+    //MAIN METHOD
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Bob" , 1.2);
+
+
+
+    }
 
     //GETTERS & SETTERS
+
+    //get/set name
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public ArrayList<Double> getGrades() {
+    //get/set array
+    public ArrayList<Integer> getGrades() {
         return grades;
     }
-    public void setGrades(ArrayList<Double> grades) {
+    public void setGrades(ArrayList<Integer> grades) {
         this.grades = grades;
     }
-    public void setGradeAverage(Double gradeAverage) {
-        this.gradeAverage = gradeAverage;
-    }
-    public Double getGradeAverage() {
-        return gradeAverage;
-    }
+
 }
