fb674d1d91d5b26e3578c81a291f5bd971323572
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-23 11:43:36 -0500

    working

--- src/contactsManager/ContactApplication.java
+++ src/contactsManager/ContactApplication.java
@@ -2,27 +2,23 @@ package contactsManager;
 
 import utils.Input;
 
-import java.io.*;
-import java.util.Arrays;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ContactApplication {
-
-    static Input input = new Input();
+    public static Input input = new Input();
 
     public static void main(String[] args) throws IOException {
-
-        //DO-WHILE CONDITION
         boolean keepLooking = true;
 
         do {
-            //PATCH TO CONTACTS
             Path contacts = Paths.get("src/contactsManager", "contacts.txt");
-            //READ CONTACTS
             List<String> contactList = Files.readAllLines(contacts);
 
             System.out.println("\n------------------ MENU -----------------------");
@@ -31,7 +27,7 @@ public class ContactApplication {
                     "\n3. Search a contact by name." +
                     "\n4. Delete an existing contact." +
                     "\n5. Exit." +
-                    "\nEnter an option(1, 2, 3, 4 or 5");
+                    "\nEnter an option (1, 2, 3, 4, 5):");
             System.out.println("-----------------------------------------------");
 
             int userChoice = input.getInt(1, 5);
@@ -40,85 +36,80 @@ public class ContactApplication {
                 case 1:
                     //VIEW CONTACTS
                     System.out.println("=============== ALL CONTACTS ==================");
+                    System.out.printf("Name%15s| Phone Number%15s|\n", "", "");
+                    System.out.println("------------------------------------------------");
+
+                    String[] allContactsArray;
                     for (String contact : contactList) {
-                        System.out.println(contact);
+
+                        allContactsArray = contact.split(",");
+                        System.out.printf("%s %s %5s| %s %5s|\n",allContactsArray[0],allContactsArray[1], " ", allContactsArray[2], " " );
+
                     }
                     keepLooking = true;
                     break;
-
                 case 2:
-                    //ADD NEW CONTACT
-                    System.out.println("Please enter contacts first name below.");
+                    //ADD CONTACT
+                    System.out.println("Enter contact first name: ");
                     String userInputFirstName = input.getString();
-                    System.out.println("Please enter contacts last name below.");
+                    System.out.println("Enter contact last name: ");
                     String userInputLastName = input.getString();
-                    System.out.println("Please enter contacts number below.");
-                    String userInputNumber = input.getString();
-                    Files.write(
-                            contacts,
-                            Arrays.asList(userInputFirstName + " " + userInputLastName + " " + userInputNumber),
-                            StandardOpenOption.APPEND
-                    );
+                    System.out.println("Enter contact phone number: ");
+                    String userInputDigits = input.getString();
+                    Files.write(contacts, Arrays.asList(userInputFirstName + ", " + userInputLastName + ", " + userInputDigits), StandardOpenOption.APPEND);
                     keepLooking = true;
                     break;
-
                 case 3:
                     //SEARCH BY NAME
-                    System.out.println("Enter contact name");
+                    System.out.println("Enter contact first name: ");
                     String userSearchContactName = input.getString();
-
                     String[] searchArray;
                     for (String contact : contactList) {
                         searchArray = contact.split(",");
 
-                        for (int i = 0; i < searchArray.length - 1; i++) {
+                        for (int i = 0; i < searchArray.length - 1; i++)
                             if (searchArray[i].contains(userSearchContactName)) {
                                 System.out.println("-------");
-                                System.out.println(searchArray[0] + searchArray[1] + " :" + searchArray[2]);
+                                System.out.println(searchArray[0] + searchArray[1] + searchArray[2]);
                                 System.out.println("-------");
                             }
-                        }
                     }
+
                     keepLooking = true;
                     break;
-
                 case 4:
-                    //OUTPUT = INPUT - DELETE
-                    System.out.println("Type in name or number of contact you wish to delete.");
+                    //DELETE CONTACT
+                    System.out.println("Enter contact First name: ");
                     String userDeleteContactName = input.getString();
 
                     String[] deleteArray;
+                    String contactToBeDeleted = "";
+
                     for (String contact : contactList) {
                         deleteArray = contact.split(",");
 
-                    List<String> deleteContact = Arrays.asList(deleteArray);
-                        for (int i = 0; i < deleteContact.size(); i++) {
-                            // if it is List<String>
-                            if (deleteContact[i].contains(i)) {
-                                deleteContact.remove(i);
-                            }
+                        List<String> deleteContact = Arrays.asList(deleteArray);
+
+                        if (deleteContact.contains(userDeleteContactName)) {
+                            contactToBeDeleted = contact;
                         }
                     }
-
-//                        for (int i = 0; i < deleteArray.length - 1; i++) {
-//                            if (deleteArray[i].contains(userDeleteContactName)) {
-//                                System.out.println("-------");
-//
-////                                System.out.println(deleteArray[0] + deleteArray[1] + " :" + deleteArray[2]);
-//                                System.out.println("-------");
-//                            }
-//                        }
-//                    }
-
+                    contactList.remove(contactToBeDeleted);
+                    Files.write(contacts, contactList);
 
                     keepLooking = true;
                     break;
                 case 5:
-                    System.out.println("While contacts may be great glasses are better! Bye!");
+                    //EXIT
+                    System.out.println("While contacts may be great glasses are better. Bye!");
                     keepLooking = false;
                     break;
+                default:
+                    System.out.println("Invalid input.");
 
             }
+
+
         } while (keepLooking);
 
 
